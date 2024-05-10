98ad7230cc6f3f3a8e712bd32ad19591dcdf23a8
atad_name <atad_email@email.com>
2024-05-10 11:22:57 +0000

    Edit /welcome-repo/git_test.sql by new promt: generate me something for staging in SQL

--- git_test.sql
+++ git_test.sql
@@ -1,3 +1,14 @@
 SELECT
   *
-FROM users
+FROM (
+  SELECT
+    T1.id AS order_id,
+    T1.created_at AS order_created_at,
+    T2.id AS user_id,
+    T2.created_at AS user_created_at,
+    ROW_NUMBER() OVER (PARTITION BY T1.id ORDER BY T2.created_at) AS user_sequence
+  FROM orders AS T1
+  INNER JOIN users AS T2
+    ON T1.user_id = T2.id
+) AS T
+WHERE T.user_sequence = 1
