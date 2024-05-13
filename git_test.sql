c2b79d45f4667dec159d895e9805bfb82d56aafb
atad_name <atad_email@email.com>
2024-05-13 15:29:43 +0000

    Delete /welcome-repo/git_test.sql manually

--- git_test.sql
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-Error: Output DSL not found or unsupported. Please make sure to end the query with 'in <DSL>' where DSL is one of: DBT, LookML, SQL, LKML
-=======
-resolved conflict on staging
->>>>>>> refs/remotes/origin/main
