4837c09f87c8cbde83c2cebee3241b8ef21834f0
atad_name <atad_email@email.com>
2024-05-13 16:29:11 +0000

    Saved /welcome-repo/joe_test.sql manually.

--- joe_test.sql
+++ joe_test.sql
@@ -1,1 +1,8 @@
-Error: Output DSL not found or unsupported. Please make sure to end the query with 'in <DSL>' where DSL is one of: DBT, LookML, SQL, LKML
+SELECT
+  T1.id AS warehouse_id,
+  SUM(T2.cost * T2.quantity) AS total_cost
+FROM distribution_centers AS T1
+INNER JOIN inventory_items AS T2
+ON T1.id = T2.distribution_center_id
+GROUP BY
+  T1.id
