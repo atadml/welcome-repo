0c31afbeb395092d2bd61c420f9a429d4ca00f8a
atad_name <atad_email@email.com>
2024-05-14 18:51:06 +0000

    Edit /welcome-repo/sample_notebook2.sql by new promt: give me orders with price above $10 in sql

--- sample_notebook2.sql
+++ sample_notebook2.sql
@@ -1,8 +1,5 @@
 SELECT
-  T1.state AS state,
-  CAST(SUM(CASE WHEN T2.status = 'Returned' THEN 1 ELSE 0 END) AS FLOAT) * 100 / COUNT(T2.status) AS return_rate
-FROM users AS T1
-INNER JOIN orders AS T2
-  ON T1.id = T2.user_id
-GROUP BY
-  T1.state
+  *
+FROM orders
+WHERE
+  sale_price > 10
