c98427e036963538b5489dad7bab5941a171c7d8
atad_name <atad_email@email.com>
2024-05-15 17:16:56 +0000

    Saved /welcome-repo/sample_notebook2.sql manually.

--- sample_notebook2.sql
+++ sample_notebook2.sql
@@ -1,5 +1,14 @@
 SELECT
-  *
-FROM orders
+  T1.product_id
+FROM inventory_items AS T1
+INNER JOIN order_items AS T2
+  ON T1.id = T2.inventory_item_id
+INNER JOIN orders AS T3
+  ON T2.order_id = T3.id
 WHERE
-  sale_price > 10
+  T3.created_at >= '2023-01-01' AND T3.created_at < '2024-01-01'
+GROUP BY
+  T1.product_id
+ORDER BY
+  SUM(T2.sale_quantity) DESC
+LIMIT 1
