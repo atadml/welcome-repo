1b2786a234e6fa1260788cb199eaa061b501a84a
atad_name <atad_email@email.com>
2024-05-28 02:26:32 +0000

    Edit /welcome-repo/sample_notebook.sql
    By promt: monthly orders

--- sample_notebook.sql
+++ sample_notebook.sql
@@ -1,2 +1,7 @@
-Error: Failed to extract metadata for your project.
-metadata_init_error_log_id: 'a0fb4a0c-3d2e-4096-809c-831721d6bbba'
+SELECT
+  EXTRACT(YEAR FROM created_at) AS order_year,
+  EXTRACT(MONTH FROM created_at) AS order_month,
+  COUNT(id) AS order_count
+FROM public.orders
+GROUP BY 1, 2
+ORDER BY 1, 2
