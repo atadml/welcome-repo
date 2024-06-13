ba4bab2c68c2e02cdbfd1514079fcbe0b4a2de1b
atad_name <atad_email@email.com>
2024-06-13 18:23:52 +0300

    Saved /welcome-repo/untitled.sql manually
    With prompt: "Content  of file"

--- untitled.sql
+++ untitled.sql
@@ -1,12 +1,5 @@
 SELECT
   *
-FROM (
-  SELECT
-    order_cohort,
-    order_day,
-    order_month,
-    order_year,
-    RANK() OVER (PARTITION BY order_cohort, order_month ORDER BY order_day) AS rank
-  FROM dbt_target_schema.sample_notebook
-) AS ranks
-WHERE rank = 1
+FROM public.users
+WHERE
+  age > 30
