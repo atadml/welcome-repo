ade9d12138678557d6c9578bddf83c3a96aaf603
atad_name <atad_email@email.com>
2024-06-13 18:28:47 +0300

    Edit /welcome-repo/untitled.sql
    With prompt: generate something simple

--- untitled.sql
+++ untitled.sql
@@ -1,12 +1,8 @@
 SELECT
   *
-FROM (
-  SELECT
-    order_cohort,
-    order_day,
-    order_month,
-    order_year,
-    RANK() OVER (PARTITION BY order_cohort, order_month ORDER BY order_day) AS rank
-  FROM dbt_target_schema.sample_notebook
-) AS ranks
-WHERE rank = 1
+FROM public.distribution_centers
+WHERE
+  country = 'USA'
+ORDER BY
+  id
+LIMIT 10
