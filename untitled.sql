864ae91e2aa9038996fe8f3e8279d098558cbb19
atad_name <atad_email@email.com>
2024-06-13 18:24:12 +0300

    Saved /welcome-repo/untitled.sql manually
    With prompt: "generate something test"

--- untitled.sql
+++ untitled.sql
@@ -1,5 +1,12 @@
 SELECT
   *
-FROM public.users
-WHERE
-  age > 30
+FROM (
+  SELECT
+    order_cohort,
+    order_day,
+    order_month,
+    order_year,
+    RANK() OVER (PARTITION BY order_cohort, order_month ORDER BY order_day) AS rank
+  FROM dbt_target_schema.sample_notebook
+) AS ranks
+WHERE rank = 1
