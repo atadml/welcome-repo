bec1c2426e44db86c86c735d8ccde3492ff9adff
atad_name <atad_email@email.com>
2024-07-04 15:02:21 +0000

    Edit /welcome-repo/workflow.sql
    By promt: I mean, I need to change the name of the returned column in this sql file

--- workflow.sql
+++ workflow.sql
@@ -1,2 +1,7 @@
-ALTER TABLE workflow
-RENAME COLUMN active_users TO active_users_new
+SELECT
+  T1.id AS id,
+  T1.name AS name,
+  T2.return_rate AS return_rate
+FROM public.distribution_centers AS T1
+INNER JOIN __dbt_target_schema__.sample_notebook2 AS T2
+  ON T1.id = T2.state
