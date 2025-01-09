e2ca323ce65397e4783124f36cea219e56bee887
atad_name <atad_email@email.com>
2025-01-09 20:19:14 +0000

    Saved /welcome-repo/untitled.sql manually
    With prompt: "what were total orders for the last 3 months"

--- untitled.sql
+++ untitled.sql
@@ -1,7 +1,8 @@
 SELECT
   EXTRACT(YEAR FROM created_at) AS year,
+  EXTRACT(MONTH FROM created_at) AS month,
   SUM(sale_price * sale_quantity) AS total_orders
 FROM orders
-GROUP BY 1
-ORDER BY 1 DESC
+GROUP BY 1, 2
+ORDER BY 1, 2 DESC
 LIMIT 3
